3e09e163d1ddb31a3061d65d6a6683b386caaa76
Sade Benjamin <skb49@pitt.edu>
2023-01-03 10:38:18 -0500

    working modal, beginning jest tests

--- cornell_todo_app/src/components/ConfirmDeleteModal.js
+++ cornell_todo_app/src/components/ConfirmDeleteModal.js
@@ -1,51 +1,33 @@
 import React from 'react';
 
-const Modal = ({ showModal, setShowModal, handleDelete }) => {
-  return (
-    <div
-      className="modal fade"
-      tabIndex="-1"
-      role="dialog"
-      style={{ display: showModal ? 'block' : 'none' }}
-    >
-      <div className="modal-dialog" role="document">
+const ConfirmDeleteModal = ({ showModal, setShowModal, handleDelete }) => {
+    return (
+      <div className="modal" style={{ display: showModal ? 'block' : 'none' }}>
         <div className="modal-content">
           <div className="modal-header">
             <h5 className="modal-title">Confirm Delete</h5>
             <button
               type="button"
               className="close"
-              data-dismiss="modal"
-              aria-label="Close"
               onClick={() => setShowModal(false)}
             >
-              <span aria-hidden="true">&times;</span>
+              &times;
             </button>
           </div>
           <div className="modal-body">
             <p>Are you sure you want to delete this task?</p>
           </div>
           <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              data-dismiss="modal"
-              onClick={() => setShowModal(false)}
-            >
+            <button type="button" className="btn btn-secondary" onClick={() => setShowModal(false)}>
               Cancel
             </button>
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={handleDelete}
-            >
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+            <button type="button" className="btn btn-danger" onClick={handleDelete}>
+            Delete
+</button>
+</div>
+</div>
+</div>
+);
 };
 
-export default Modal;
+export default ConfirmDeleteModal;
